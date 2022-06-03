227540747f1820f19b04cf9bba66aa54c83aa180
badytouray <badytouray@gmail.com>
2022-06-03 20:55:52 +0100

    started React reveal

--- src/Components/Interviews/RampaigeInterview/RampaigeInterview.js
+++ src/Components/Interviews/RampaigeInterview/RampaigeInterview.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from '../../Footer/Footer';
 import MaddyV from "../../Images/MaddyV.jpeg";
 import "./RampaigeInterview.css"
+import { Fade, Flip, Rotate, Zoom } from 'react-reveal';
 
 function RampaigeInterview() {
   return (
@@ -41,8 +42,9 @@ function RampaigeInterview() {
 
                          {/* Question 4 */}
     <p className="interview_questions">What made you think you can be an MC?</p>
+    <Fade left>
     <img src={MaddyV} alt="Snow" style={{width: "30%", float: "left", height: "30%", padding: "10px", margin: "10px"}} />
-    
+    </Fade>
     <div class="container-fluid p-4 pb-0">
    
 
@@ -57,8 +59,10 @@ function RampaigeInterview() {
 
                         {/* Question 6 */}
     <p className="interview_questions">What made you think you can be an MC?</p>
-    <p className="interview_answers">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+    <p className="interview_answers">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard MPeventcClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+    <Fade right>
     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen style={{margin: "10px", height: "300px", width: "400px", float: "right"}}></iframe>
+    </Fade>
     <br />
 
                         {/* Question 7 */}
